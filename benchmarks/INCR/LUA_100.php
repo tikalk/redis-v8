f30cf88a5737730add2f5348e4d83823be4c2ab3
h0x91b <h0x91b@gmail.com>
2013-11-01 12:33:03 +0200

    [+] INIT

---
+++ benchmarks/INCR/LUA_100.php
@@ -0,0 +1,18 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+require '../../Client-Libraries/PHP/predis-0.8.3/Autoloader.php';
+Predis\Autoloader::register();
+
+$redis = new Predis\Client('unix:///tmp/redis.sock');
+
+$redis->set('bench_key','1');
+
+$sha1 = $redis->script("LOAD","local a = 0 for i=1, 100 do a = redis.call('INCR','bench_key') end return a");
+
+$start = microtime(TRUE);
+for($i=0;$i<100000/100;$i++){
+	$redis->evalsha($sha1,0);
+}
+echo "Speed: ".(round(100000/(microtime(TRUE)-$start)))." ops/sec\n";
+$redis->del('bench_key');
+?>
---
+++ benchmarks/SET/LUA_10.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+require '../../Client-Libraries/PHP/predis-0.8.3/Autoloader.php';
+Predis\Autoloader::register();
+
+$redis = new Predis\Client('unix:///tmp/redis.sock');
+
+$sha1 = $redis->script("LOAD","local a = {} for i=1, 10 do a[i] = redis.call('SET','bench_key', 'value') end");
+
+$start = microtime(TRUE);
+for($i=0;$i<100000/10;$i++){
+	$redis->evalsha($sha1,0);
+}
+echo "Speed: ".(round(100000/(microtime(TRUE)-$start)))." ops/sec\n";
+$redis->del('bench_key');
+?>
